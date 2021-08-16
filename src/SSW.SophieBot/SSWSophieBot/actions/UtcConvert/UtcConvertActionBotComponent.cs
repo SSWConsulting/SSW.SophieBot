6de8f8ebdec6df1980265c09503173a7d49c5549
Jim Zheng <JimZheng@ssw.com.au>
2021-08-16 12:02:48 +0800

    Add Utc convert custom action

---
+++ src/SSW.SophieBot/SSWSophieBot/actions/UtcConvert/UtcConvertActionBotComponent.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs.Declarative;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SSWSophieBot.actions.UtcConvert
+{
+    public class UtcConvertActionBotComponent : BotComponent
+    {
+        public override void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddSingleton<DeclarativeType>(sp => new DeclarativeType<UtcConvertAction>(UtcConvertAction.Kind));
+        }
+    }
+}
