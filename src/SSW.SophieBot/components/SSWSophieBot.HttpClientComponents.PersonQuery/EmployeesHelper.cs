4f1b8e6bceccd5310fe54b2edc0a85bc4d7adbfb
pfmao <ErenJagerM@outlook.com>
2021-09-17 11:33:43 +0800

    fix: query error

--- src/SSW.SophieBot/components/SSWSophieBot.HttpClientComponents.PersonQuery/EmployeesHelper.cs
+++ src/SSW.SophieBot/components/SSWSophieBot.HttpClientComponents.PersonQuery/EmployeesHelper.cs
@@ -87,20 +87,20 @@ namespace SSWSophieBot.HttpClientComponents.PersonQuery
         {
             var now = DateTime.Now.ToUniversalTime();
             var appointments = employee.Appointments
-                .Where(appointment => appointment.Start.UtcDateTime.Ticks > now.Ticks && appointment.Regarding.ToLower() != "ssw")
+                .Where(appointment => appointment.Start.UtcDateTime.Ticks > now.Ticks && appointment.Regarding?.ToLower() != "ssw")
                 .ToList();
+            var appointment = appointments.LastOrDefault();
 
-            if (appointments.Count == 0)
+            if (appointments.Count == 0 || appointment.Regarding == null)
             {
                 return null;
             }
 
-            var appointment = appointments.LastOrDefault();
             return new NextClientModel
             {
                 FreeDays = GetBusinessDays(DateTime.Now.ToUniversalTime(), appointment.Start.UtcDateTime),
                 Name = appointment.Regarding,
-                Date = appointment.Start.ToString("'ddd', dd/MM/yyyy")
+                Date = appointment.Start.ToString("ddd, dd/MM/yyyy")
             };
 
             static int GetBusinessDays(DateTime start, DateTime end)
