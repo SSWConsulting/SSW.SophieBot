0c195ce15fa83529b25b48db2b05cba488a0a555
Jim Zheng <JimZheng@ssw.com.au>
2021-10-21 17:48:37 +0800

    Add free for booking status, refinements and bug fixes

--- src/SSW.SophieBot/components/SSWSophieBot.HttpClientComponents.PersonQuery/Actions/GetFreeEmployeesAction.cs
+++ src/SSW.SophieBot/components/SSWSophieBot.HttpClientComponents.PersonQuery/Actions/GetFreeEmployeesAction.cs
@@ -47,7 +47,7 @@ namespace SSWSophieBot.HttpClientComponents.PersonQuery.Actions
                 ? EmployeesHelper.ToUserLocalTime(dc, DateTime.Parse(dateString)).AddHours(9)
                 : DateTime.Now.ToUniversalTime();
 
-            var result = EmployeesHelper.FilterEmployees(employees).Select(employee => new FreeEmployeeModel
+            var result = EmployeesHelper.FilterDevelopers(employees).Select(employee => new FreeEmployeeModel
             {
                 FirstName = employee.FirstName,
                 LastName = employee.LastName,
