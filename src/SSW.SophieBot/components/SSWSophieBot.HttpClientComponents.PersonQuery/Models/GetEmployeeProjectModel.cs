628592b030aacac9390bf56f0698c437d03a0482
Jim Zheng <JimZheng@ssw.com.au>
2021-11-10 11:26:35 +0800

    Make More Project Info button directs to Crm project details page

--- src/SSW.SophieBot/components/SSWSophieBot.HttpClientComponents.PersonQuery/Models/GetEmployeeProjectModel.cs
+++ src/SSW.SophieBot/components/SSWSophieBot.HttpClientComponents.PersonQuery/Models/GetEmployeeProjectModel.cs
@@ -3,6 +3,7 @@
     public class GetEmployeeProjectModel
     {
         public string ProjectId { get; set; }
+        public string CrmProjectId { get; set; }
         public string ProjectName { get; set; }
         public string CustomerName { get; set; }
         public double BillableHours { get; set; }
--- src/SSW.SophieBot/components/SSWSophieBot.HttpClientComponents.PersonQuery/Models/ProjectWithEmployeesCountModel.cs
+++ src/SSW.SophieBot/components/SSWSophieBot.HttpClientComponents.PersonQuery/Models/ProjectWithEmployeesCountModel.cs
@@ -3,15 +3,19 @@
 namespace SSWSophieBot.HttpClientComponents.PersonQuery.Models
 {
     public class ProjectWithEmployeesCountModel
-    {
+    { 
+        [JsonProperty("crmProjectId")]
+        public string CrmProjectId { get; set; }
+
         [JsonProperty("projectName")]
         public string ProjectName { get; set; }
 
         [JsonProperty("employeesCount")]
         public int EmployeesCount { get; set; }
 
-        public ProjectWithEmployeesCountModel(string projectName, int employeeCount)
+        public ProjectWithEmployeesCountModel(string crmProjectId, string projectName, int employeeCount)
         {
+            CrmProjectId = crmProjectId;
             ProjectName = projectName;
             EmployeesCount = employeeCount;
         }
