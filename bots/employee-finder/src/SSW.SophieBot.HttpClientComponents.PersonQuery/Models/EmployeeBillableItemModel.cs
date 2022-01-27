2671dff4f1f77bc1438f51ab35d3c5982733fbc7
Ravi Dhaduk <ravidhaduk1611@gmail.com>
2022-01-27 12:53:57 +1100

    Update UI

--- bots/employee-finder/src/SSW.SophieBot.HttpClientComponents.PersonQuery/Models/EmployeeBillableItemModel.cs
+++ bots/employee-finder/src/SSW.SophieBot.HttpClientComponents.PersonQuery/Models/EmployeeBillableItemModel.cs
@@ -1,34 +1,45 @@
 ﻿using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace SSW.SophieBot.HttpClientComponents.PersonQuery.Models
 {
-    public class EmployeeBillableItemModel
-    {
-        [JsonProperty("userId")]
-        public string UserId { get; set; }
+	public class EmployeeBillableItemModel
+	{
+		[JsonProperty("userId")]
+		public string UserId { get; set; }
+		[JsonProperty("avatarUrl")]
+		public string AvatarUrl { get; set; }
+		[JsonProperty("firstName")]
+		public string FirstName { get; set; }
+		[JsonProperty("lastName")]
+		public string LastName { get; set; }
+		[JsonProperty("displayName")]
+		public string DisplayName { get; set; }
+		[JsonProperty("billedDays")]
+		public int BilledDays { get; set; }
+		[JsonProperty("billedHours")]
+		public int BilledHours { get; set; }
+		[JsonProperty("billedProjects")]
+		public List<BilledProject> BilledProjects { get; set; }
+		[JsonProperty("bookingStatus")]
+		public BookingStatus BookingStatus { get; set; }
+		[JsonProperty("lastSeen")]
+		public string LastSeen { get; set; }
+	}
+	public class BilledProject
+	{
+		[JsonProperty("billedDays")]
+		public int BilledDays { get; set; }
+		[JsonProperty("billedHours")]
+		public int BilledHours { get; set; }
+		[JsonProperty("projectName")]
+		public string ProjectName { get; set; }
+		[JsonProperty("projectId")]
+		public string ProjectId { get; set; }
+		[JsonProperty("customerName")]
+		public string CustomerName { get; set; }
+		[JsonProperty("crmProjectId")]
+		public string CrmProjectId { get; set; }
 
-        [JsonProperty("avatarUrl")]
-        public string AvatarUrl { get; set; }
-
-        [JsonProperty("firstName")]
-        public string FirstName { get; set; }
-
-        [JsonProperty("lastName")]
-        public string LastName { get; set; }
-
-        [JsonProperty("displayName")]
-        public string DisplayName { get; set; }
-
-        [JsonProperty("billedDays")]
-        public int BilledDays { get; set; }
-
-        [JsonProperty("billedHours")]
-        public int BilledHours { get; set; }
-
-        [JsonProperty("bookingStatus")]
-        public BookingStatus BookingStatus { get; set; }
-
-        [JsonProperty("lastSeen")]
-        public string LastSeen { get; set; }
-    }
+	}
 }
